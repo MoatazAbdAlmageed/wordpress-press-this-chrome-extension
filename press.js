73bd250f4d14ef038e281877fcd413824dc5300c
Moataz Mohammady <moataz@wuilt.com>
2021-04-28 15:35:11 +0200

    add image

--- press.js
+++ press.js
@@ -3,21 +3,26 @@
     (getDocSelection = document.getSelection),
     (selection = document.selection),
     (loc = document.location);
-  const url =
-      e +
-      "?url=" +
-      encodeURIComponent(loc.href) +
-      "&title=" +
-      encodeURIComponent(document.title) +
-      "&text=" +
-      encodeURIComponent(document.URL) +
-      "&v=5",
-    c = function () {
-      window.open(
-        url,
-        "t",
-        "toolbar=0,resizable=1,scrollbars=1,status=1,width=660,height=570"
-      ) || (loc.href = url);
-    };
+  const image = document.querySelector("#channel-header-container img").src;
+
+  const href = encodeURIComponent(loc.href);
+  const title = encodeURIComponent(document.title);
+  const text = encodeURIComponent(document.URL);
+  console.log("\n".repeat(5));
+  console.log("************ image **************");
+  console.log(image);
+  const url = `${e}?url=${href}?i=${image}&title=${title}&text=${text}&v=5`;
+
+  console.log("\n".repeat(5));
+  console.log("************ url **************");
+  console.log(url);
+  const c = function () {
+    window.open(
+      url,
+      "t",
+      "i",
+      "toolbar=0,resizable=1,scrollbars=1,status=1,width=660,height=570"
+    ) || (loc.href = url);
+  };
   /Firefox/.test(navigator.userAgent) ? setTimeout(c, 0) : c();
 })("https://wordpress.com/post/arabicyoutubechannels.wordpress.com");
